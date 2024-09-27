3983ae4cff88ad6fc59d600dddfeb3b42f984cc9
marzwn <58574525+marzwn@users.noreply.github.com>
2024-09-27 14:57:23 -0400

    Improved Qubits

---
+++ Assets/Scripts/Qubit.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics;
+using static QubitManager;
+using static Gates;
+public class Qubit : MonoBehaviour
+{
+    private Matrix<Complex32> identityMatrix = IdentityMatrix();
+    private Matrix<Complex32> pauliX = PauliX();
+    private Matrix<Complex32> pauliZ = PauliZ();
+    private Matrix<Complex32> hadamard = Hadamard();
+    private Matrix<Complex32> phaseS = PhaseS();
+    private Matrix<Complex32> phaseSDagger = PhaseSDagger();
+    public void Start()
+    {
+        IncrementInitQubits();
+        int initQubits = GetInitQubits();
+        for (int i = 1; i < initQubits; i++)
+        {
+            identityMatrix = IdentityMatrix().KroneckerProduct(identityMatrix);
+            pauliX = IdentityMatrix().KroneckerProduct(pauliX);
+            pauliZ = IdentityMatrix().KroneckerProduct(pauliZ);
+            hadamard = IdentityMatrix().KroneckerProduct(hadamard);
+            phaseS = IdentityMatrix().KroneckerProduct(phaseS);
+            phaseSDagger = IdentityMatrix().KroneckerProduct(phaseSDagger);
+        }
+
+        for (int i = 0; i < GetQubits() - initQubits; i++)
+        {
+            identityMatrix = identityMatrix.KroneckerProduct(IdentityMatrix());
+            pauliX = pauliX.KroneckerProduct(IdentityMatrix());
+            pauliZ = pauliZ.KroneckerProduct(IdentityMatrix());
+            hadamard = hadamard.KroneckerProduct(IdentityMatrix());
+            phaseS = phaseS.KroneckerProduct(IdentityMatrix());
+            phaseSDagger = phaseSDagger.KroneckerProduct(IdentityMatrix());
+        }
+    }
+    public Matrix<Complex32> GetIdentityMatrix()
+    {
+        return identityMatrix;
+    }
+    public Matrix<Complex32> GetPauliX()
+    {
+        return pauliX;
+    }
+    public Matrix<Complex32> GetPauliZ()
+    {
+        return pauliZ;
+    }
+    public Matrix<Complex32> GetHadamard()
+    {
+        return hadamard;
+    }
+
+    public Matrix<Complex32> GetPhaseS()
+    {
+        return phaseS;
+    }
+    public Matrix<Complex32> GetPhaseSDagger()
+    {
+        return phaseSDagger;
+    }
+
+}
---
+++ Assets/Scripts/QubitManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics;
+using static Gates;
+public class QubitManager : MonoBehaviour
+{
+    static private Matrix<Complex32> densityMatrix = Matrix<Complex32>.Build.DenseOfArray(new Complex32[,]
+    {
+        { 1, 0 },
+        { 0, 0 }
+    });
+    static int numQubits = 0;
+    static int initQubits = 0;
+    public void Awake()
+    {
+        Qubit[] qubits = FindObjectsOfType<Qubit>();
+        for (int i = 1; i < qubits.Length; i++)
+        {
+            densityMatrix = densityMatrix.KroneckerProduct(UpMatrix());
+        }
+        numQubits = qubits.Length;
+    }
+
+    public static Matrix<Complex32> GetDensityMatrix()
+    {
+        return densityMatrix;
+    }
+    public static int GetQubits()
+    {
+        return numQubits;
+    }
+
+    public static int GetInitQubits()
+    {
+        return initQubits;
+    }
+
+    public static void IncrementInitQubits()
+    {
+        initQubits += 1;
+    }
+
+    public static void ApplyGate(Matrix<Complex32> gate)
+    {
+        densityMatrix = gate * densityMatrix * gate;
+    }
+}
