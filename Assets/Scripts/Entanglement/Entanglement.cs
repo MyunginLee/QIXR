611ba7199b9ec371eeb5b6f8febaf07e9a5c40f8
Myungin Lee <benedic.lee@gmail.com>
2024-10-15 20:02:10 -0400

    Fixed the collision between qubit and strings

--- Assets/Scripts/Entanglement/Entanglement.cs
+++ Assets/Scripts/Entanglement/Entanglement.cs
@@ -7,8 +7,9 @@ public class Entanglement : MonoBehaviour
     private const float G = 500f;
 
     GameObject[] strings;
+
     BodyProperty[] bp;
-    private int numberOfSphere = 300;
+    private int numberOfStrings = 300;
     TrailRenderer trailRenderer;
     GameObject[] qubits;
     GameObject[] sphere;
@@ -26,30 +27,31 @@ public class Entanglement : MonoBehaviour
     void Start()
     {
         qubits = GameObject.FindGameObjectsWithTag("Core");
-        bp = new BodyProperty[numberOfSphere];
-        strings = new GameObject[numberOfSphere];
+        bp = new BodyProperty[numberOfStrings];
+        strings = new GameObject[numberOfStrings];
         entangled = new bool[qubits.Length];
-        for (int i = 0; i < numberOfSphere; i++)
+        for (int i = 0; i < numberOfStrings; i++)
         {
             strings[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            strings[i].GetComponent<Collider>().isTrigger = true;
             float r = 10f;
             strings[i].transform.localScale = new Vector3(0.00001f, 0.00001f, 0.00001f);
-            strings[i].transform.position = new Vector3(r * Mathf.Cos(Mathf.PI * 2 / numberOfSphere * i), r * Mathf.Sin(Mathf.PI * 2 / numberOfSphere * i), Random.Range(-10, 10));
+            strings[i].transform.position = new Vector3(r * Mathf.Cos(Mathf.PI * 2 / numberOfStrings * i), r * Mathf.Sin(Mathf.PI * 2 / numberOfStrings * i), Random.Range(-10, 10));
             bp[i].velocity = new Vector3(r / 10f * Mathf.Sin(Mathf.PI * 2f / 3f * i), r / 10f * Mathf.Cos(Mathf.PI * 2f / 3f * i), 0);
 
             // trail
             trailRenderer = strings[i].AddComponent<TrailRenderer>();
             // Configure the TrailRenderer's properties
             trailRenderer.time = trailtime;  // Duration of the trail
-            trailRenderer.startWidth = 0.01f;  // Width of the trail at the start
-            trailRenderer.endWidth = 0.01f;    // Width of the trail at the end
+            trailRenderer.startWidth = 0.003f;  // Width of the trail at the start
+            trailRenderer.endWidth = 0.003f;    // Width of the trail at the end
             // a material to the trail
             trailRenderer.material = new Material(Shader.Find("Sprites/Default"));
             // Set the trail color over time
             Gradient gradient = new Gradient();
             gradient.SetKeys(
-                //new GradientColorKey[] { new GradientColorKey(new Color((Mathf.Sin(Mathf.PI * 2f / numberOfSphere * i) + 1) / 2f, (Mathf.Cos(Mathf.PI * 2f / numberOfSphere * i) + 1) / 2f, Mathf.Tan(Mathf.PI * 2 / numberOfSphere * i)), 0.80f), new GradientColorKey(new Color((Mathf.Cos(Mathf.PI * 2 / numberOfSphere * i) + 1) / 5f, (Mathf.Sin(Mathf.PI * 2 / numberOfSphere * i) + 1) / 3f, Mathf.Tan(Mathf.PI * 2 / numberOfSphere * i)), 0.05f) },
-                new GradientColorKey[] { new GradientColorKey(new Color(0f,0f, 0f), 0f), new GradientColorKey(new Color((Mathf.Cos(Mathf.PI * 2 / numberOfSphere * i) + 1) / 3f, 0.2f - (Mathf.Cos(Mathf.PI * 2 / numberOfSphere * i) / 1.5f), 0.3f+(Mathf.Sin(Mathf.PI * 2 / numberOfSphere * i) + 1) / 3f), 0.05f) },
+                //new GradientColorKey[] { new GradientColorKey(new Color((Mathf.Sin(Mathf.PI * 2f / numberOfStrings * i) + 1) / 2f, (Mathf.Cos(Mathf.PI * 2f / numberOfStrings * i) + 1) / 2f, Mathf.Tan(Mathf.PI * 2 / numberOfStrings * i)), 0.80f), new GradientColorKey(new Color((Mathf.Cos(Mathf.PI * 2 / numberOfStrings * i) + 1) / 5f, (Mathf.Sin(Mathf.PI * 2 / numberOfStrings * i) + 1) / 3f, Mathf.Tan(Mathf.PI * 2 / numberOfStrings * i)), 0.05f) },
+                new GradientColorKey[] { new GradientColorKey(new Color(0f,0f, 0f), 0f), new GradientColorKey(new Color((float)(Mathf.Cos(Mathf.PI * 2f / numberOfStrings * i) + 1f) / 3f, 0.2f - (float)(Mathf.Cos(Mathf.PI * 2f / numberOfStrings * i) / 1.5f), 0.3f+ (float)(Mathf.Sin(Mathf.PI * 2f / numberOfStrings * i) + 1f) / 3f), 0.05f) },
                 new GradientAlphaKey[] { new GradientAlphaKey(0.1f, 0.1f), new GradientAlphaKey(0.1f, 0.1f) }
             );
             trailRenderer.colorGradient = gradient;
@@ -58,7 +60,7 @@ public class Entanglement : MonoBehaviour
 
     void Update()
     {
-        for (int i = 0; i < numberOfSphere; i++)
+        for (int i = 0; i < numberOfStrings; i++)
         {
             bp[i].acceleration = Vector3.zero;
         }
@@ -85,7 +87,7 @@ public class Entanglement : MonoBehaviour
             {
                 float qubitScale = QubitManager.J[j] / 3.15f;
                 qubits[j].transform.localScale  = new Vector3(qubitScale, qubitScale, qubitScale);
-                for (int i = 0; i < numberOfSphere; i++)
+                for (int i = 0; i < numberOfStrings; i++)
                 {
                     trailRenderer.time = trailtime;
                     Vector3 distance = qubits[j].transform.position - strings[i].transform.position;
@@ -115,16 +117,21 @@ public class Entanglement : MonoBehaviour
                     // Trail color according to J
                     Gradient gradient = new Gradient();
                     gradient.SetKeys(
-                        //new GradientColorKey[] { new GradientColorKey(new Color((Mathf.Sin(Mathf.PI * 2f / numberOfSphere * i) + 1) / 2f, (Mathf.Cos(Mathf.PI * 2f / numberOfSphere * i) + 1) / 2f, Mathf.Tan(Mathf.PI * 2 / numberOfSphere * i)), 0.80f), new GradientColorKey(new Color((Mathf.Cos(Mathf.PI * 2 / numberOfSphere * i) + 1) / 5f, (Mathf.Sin(Mathf.PI * 2 / numberOfSphere * i) + 1) / 3f, Mathf.Tan(Mathf.PI * 2 / numberOfSphere * i)), 0.05f) },
-                        new GradientColorKey[] { new GradientColorKey(new Color(0f, 0f, 0f), 0f), new GradientColorKey(new Color((Mathf.Cos(Mathf.PI * 2 / numberOfSphere * i) + 1) / 5f, 0.1f - (Mathf.Cos(Mathf.PI * 2 / numberOfSphere * i) / 10f), 0.5f + (Mathf.Sin(Mathf.PI * 2 / numberOfSphere * i)) / 9f), QubitManager.J[j] / 10f) },
-                        new GradientAlphaKey[] { new GradientAlphaKey(0.1f, 0.1f), new GradientAlphaKey(QubitManager.J[j] / 5f, QubitManager.J[j] / 5f) }
+                        //new GradientColorKey[] { new GradientColorKey(new Color((Mathf.Sin(Mathf.PI * 2f / numberOfStrings * i) + 1) / 2f, (Mathf.Cos(Mathf.PI * 2f / numberOfStrings * i) + 1) / 2f, Mathf.Tan(Mathf.PI * 2 / numberOfStrings * i)), 0.80f), new GradientColorKey(new Color((Mathf.Cos(Mathf.PI * 2 / numberOfStrings * i) + 1) / 5f, (Mathf.Sin(Mathf.PI * 2 / numberOfStrings * i) + 1) / 3f, Mathf.Tan(Mathf.PI * 2 / numberOfStrings * i)), 0.05f) },
+                        new GradientColorKey[] { new GradientColorKey(new Color(0f, 0f, 0f), 0f), 
+                            new GradientColorKey(new Color((float)(Mathf.Cos(Mathf.PI * 2f / (float)numberOfStrings * i) + 1f) / 5f, 
+                                                                   0.1f - (float)(Mathf.Cos(Mathf.PI * 2f / (float)numberOfStrings * i) / 10f), 
+                                                                   0.5f + (float)(Mathf.Sin(Mathf.PI * 2f / (float)numberOfStrings * i)) / 9f), 
+                                                                   QubitManager.J[j] / 10f) },
+                        new GradientAlphaKey[] { new GradientAlphaKey(0.1f, 0.1f), 
+                            new GradientAlphaKey(QubitManager.J[j] / 5f, QubitManager.J[j] / 5f) }
                     );
                     trailRenderer.colorGradient = gradient;
                 }
             }
             else // unentangled
             {
-                for (int i = 0; i < numberOfSphere; i++)
+                for (int i = 0; i < numberOfStrings; i++)
                 {
                     bp[i].acceleration = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
                     bp[i].velocity += bp[i].acceleration * Time.deltaTime;
